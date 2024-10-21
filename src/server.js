013a707cd03efaf394a342882dad09465400e9fc
Tuan <trinhsytuan@gmail.com>
2024-10-21 14:54:29 +0700

    update server router

--- src/server.js
+++ src/server.js
@@ -7,7 +7,8 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors({ credentials: true, origin: "*" }));
-
+const environmentRouter = require('./router/environmentRouter');
+app.use("/api", environmentRouter);
 
 async function main() {
   try {
